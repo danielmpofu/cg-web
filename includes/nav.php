18faee8f6b9836aa3374dab255fc68cf5103d9f6
Daniel T Mpofu <danielmpofu@hotmail.com>
2022-12-29 11:14:00 +0200

    cpanel yml

--- includes/nav.php
+++ includes/nav.php
@@ -6,27 +6,27 @@
 
 <head>
     <!-- ========== Meta Tags ========== -->
-<!--    <meta charset="UTF-8">-->
-<!--    <meta http-equiv="X-UA-Compatible" content="IE=edge">-->
-<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
-<!--    <meta name="author" content="modinatheme">-->
-<!--    <!-- ======== Page title ============ -->-->
-<!--    <title>CONSULTER - Business Consulting HTML Template</title>-->
-<!--    <!-- ========== Favicon Icon ========== -->-->
-<!--    <link rel="shortcut icon" href="assets/img/favicon.png">-->
-<!--    <!-- ===========  All Stylesheet ================= -->-->
-<!--    <!--  Icon css plugins -->-->
-<!--    <link rel="stylesheet" href="assets/css/icons.css">-->
-<!--    <!--  magnific-popup css plugins -->-->
-<!--    <link rel="stylesheet" href="assets/css/magnific-popup.css">-->
-<!--    <!-- slick slider menu css file -->-->
-<!--    <link rel="stylesheet" href="assets/css/slick.min.css">-->
-<!--    <!-- animate animation css file -->-->
-<!--    <link rel="stylesheet" href="assets/css/animate.min.css">-->
-<!--    <!--  Bootstrap css plugins -->-->
-<!--    <link rel="stylesheet" href="assets/css/bootstrap.min.css">-->
-<!--    <!-- template main style css file -->-->
-<!--    <link rel="stylesheet" href="style.css">-->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="modinatheme">
+    <!-- ======== Page title ============ -->
+    <title>CONSULTER - Business Consulting HTML Template</title>
+    <!-- ========== Favicon Icon ========== -->
+    <link rel="shortcut icon" href="assets/img/favicon.png">
+    <!-- ===========  All Stylesheet ================= -->
+    <!--  Icon css plugins -->
+    <link rel="stylesheet" href="assets/css/icons.css">
+    <!--  magnific-popup css plugins -->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <!-- slick slider menu css file -->
+    <link rel="stylesheet" href="assets/css/slick.min.css">
+    <!-- animate animation css file -->
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <!--  Bootstrap css plugins -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- template main style css file -->
+    <link rel="stylesheet" href="style.css">
 
 </head>
 
