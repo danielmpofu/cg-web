7a60b185c6fcb04e6de05c7ebb1297d6a91bd0c7
Daniel T Mpofu <danielmpofu@hotmail.com>
2022-12-29 10:18:19 +0200

    Update nav.php
    removed loader thingy

--- includes/nav.php
+++ includes/nav.php
@@ -31,63 +31,8 @@
 </head>
 
 <body class="body-wrapper">
-<!-- preloader -->
-<div id="preloader" class="preloader">
-    <div class="animation-preloader">
-        <div class="spinner">
-        </div>
-        <div class="txt-loading">
-                    <span data-text-preloader="C" class="letters-loading">
-                        C
-                    </span>
-            <span data-text-preloader="O" class="letters-loading">
-                        O
-                    </span>
-            <span data-text-preloader="N" class="letters-loading">
-                    N
-                    </span>
-            <span data-text-preloader="S" class="letters-loading">
-                    S
-                    </span>
-            <span data-text-preloader="U" class="letters-loading">
-                        U
-                    </span>
-            <span data-text-preloader="L" class="letters-loading">
-                        L
-                    </span>
-            <span data-text-preloader="T" class="letters-loading">
-                        T
-                    </span>
-            <span data-text-preloader="E" class="letters-loading">
-                        E
-                    </span>
-            <span data-text-preloader="R" class="letters-loading">
-                        R
-                    </span>
-        </div>
-        <p class="text-center">Loading</p>
-    </div>
-    <div class="loader">
-        <div class="row">
-            <div class="col-3 loader-section section-left">
-                <div class="bg"></div>
-            </div>
-            <div class="col-3 loader-section section-left">
-                <div class="bg"></div>
-            </div>
-            <div class="col-3 loader-section section-right">
-                <div class="bg"></div>
-            </div>
-            <div class="col-3 loader-section section-right">
-                <div class="bg"></div>
-            </div>
-        </div>
-    </div>
-</div>
 
-<!-- welcome content start from here -->
 
-<!-- header end -->
 <header class="header header-1 transparent header-2">
     <div class="top-header d-none d-xl-block">
         <div class="container">
